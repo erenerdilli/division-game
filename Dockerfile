ad999e4873f42e8495e528afc5f91f11dd5f5c84
erenerdilli <erenerdilli@gmail.com>
2021-11-10 21:45:31 +0200

    changing and deleting files

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM adoptopenjdk/openjdk11:alpine-jre
-
-RUN mvn clean install
-
-COPY target/divisiongame-service1.jar app.jar
-
-EXPOSE 8080
-
-ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/takeaway/divisiongame/gameplay/StaticVariables.java
+++
@@ -1,7 +0,0 @@
-package com.takeaway.divisiongame.gameplay;
-
-public class StaticVariables {
-
-    public static boolean START_OF_GAME = true;
-    public static int PLAYER_NUMBER = 0;
-}
