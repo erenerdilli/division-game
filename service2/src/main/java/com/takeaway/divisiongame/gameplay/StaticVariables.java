93eeecc63a46384d4a2db1b9b8de0f91d9454bd3
erenerdilli <erenerdilli@gmail.com>
2021-11-11 15:05:56 +0200

    further improving services and changing docker-compose.yml

--- service2/src/main/java/com/takeaway/divisiongame/gameplay/StaticVariables.java
+++ service2/src/main/java/com/takeaway/divisiongame/gameplay/StaticVariables.java
@@ -2,6 +2,6 @@ package com.takeaway.divisiongame.gameplay;
 
 public class StaticVariables {
 
-    public static boolean START_OF_GAME = true;
+    public static boolean START_OF_GAME = false;
     public static int PLAYER_NUMBER = 0;
 }
